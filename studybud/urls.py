d0498955040e96692fd8e5a7427ca17f67be689b
maisha1209 <maishaa2309@gmail.com>
2024-09-13 16:04:10 -0400

    Templates

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,8 +1,21 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
+
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls'))
+    
 ]
+
+
+
+
 #my name is python
